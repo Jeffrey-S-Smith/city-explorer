d766781ed579733b087478381e11006dad3addd0
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-08-21 09:36:57 -0700

    setup

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+           <p>Code Fellows by Jeffrey Smith 2022</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+
+class Main extends React.Component {
+  render() {
+    return(
+      <main/>
+    );
+  }
+}
+
+export default Main;
